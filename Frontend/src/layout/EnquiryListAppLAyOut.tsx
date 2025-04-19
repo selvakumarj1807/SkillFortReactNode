af2ce96738b6905c261c6b071a5b3a717bf280a6
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-04-19 15:35:15 +0530

    add AppHeader for Enquiry List

--- Frontend/src/layout/EnquiryListAppLAyOut.tsx
+++ Frontend/src/layout/EnquiryListAppLAyOut.tsx
@@ -1,6 +1,6 @@
 import { SidebarProvider, useSidebar } from "../context/SidebarContext";
 import { Outlet } from "react-router";
-import AppHeader from "./AppHeader";
+import AppHeader from "./AppHeaderEnquiryList";
 import Backdrop from "./Backdrop";
 import AppSidebar from "./EnquiryListSideBar";
 
